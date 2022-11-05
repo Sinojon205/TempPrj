bb101d7c4629285ff13e08c826332418f6a91fa3
Sinojon205 <sinojon205@gmail.com>
2022-11-05 08:57:32 +0500

    dsf

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Text");
+            MessageBox.Show("Khush omaded");
         }
     }
 }
